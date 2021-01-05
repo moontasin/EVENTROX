7c93def1ce9cfe3024d5cd9802db3ed1aab79562
moontasin <me@moontasin.com>
2021-01-05 15:55:44 +0600

    eventrox

---
+++ js/script.js
@@ -0,0 +1,103 @@
+
+// ============stiky bar===============
+$(window).scroll(function() {
+    var scrollamount = $(window).scrollTop()
+  
+    if (scrollamount > 150) {
+      $(".menu").addClass("fixed")
+    } else {
+      $(".menu").removeClass("fixed")
+    }
+  
+    if (scrollamount > 150) {
+      $(".btop").fadeIn();
+    } else {
+      $(".btop").fadeOut();
+    }
+  })
+  
+
+// animated js
+new WOW().init();
+
+// banner js
+$('.banners').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: '<i class="fa fa-chevron-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-chevron-right next" aria-hidden="true"></i>',
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+// counter js
+
+  // Set the date we're counting down to
+  var countDownDate = new Date("dec 14, 2021 23:59:00").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+  
+    // Get today's date and time
+    var now = new Date().getTime();
+  
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="demo"
+    document.getElementById("day").innerHTML = days
+    document.getElementById("hour").innerHTML = hours
+    document.getElementById("min").innerHTML = minutes
+    document.getElementById("sec").innerHTML = seconds
+  
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+ $('.venobox').venobox(); 
